76d65945ff0f4302903ae8d8c4fe4b22e733682d
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-05-05 03:46:16 +0300

    DONE WITH ASSIGNMENT

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
